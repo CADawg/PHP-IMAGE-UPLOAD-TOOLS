4fcf3c797da1397eb5fb7e085f6be2e1a4ae431f
Conor  H <conor.howland@outlook.com>
2017-08-08 10:47:43 +0100

    Added a lot more customiseability
    Added $target,$ts,$uploadedTo and $replTxt

--- tools.php
+++ tools.php
@@ -23,21 +23,21 @@ function img_resize($target, $newcopy, $w, $h) {
     imagejpeg($tci, $newcopy, 84);
 }
 
-function isImageWithUpload($user) {
-			//File Must Be $_FILES['fileToUpload']
-			$tofile = $user . "_". time() . "_orig";
-			$target_dir = "pictures/profiles/";
+function isImageWithUpload($user,$target,$ts,$uploadedTo) {
+			if ($ts) {$tofile = $user . "_". time() . "_orig";} else {$tofile = $user . "_orig";}
+			if (!isset($uploadedTo) { $uploadedTo = "fileToUpload";}
+			$target_dir = $target;
 			$target_file = $target_dir . $tofile;
-			$imageFileType = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+			$imageFileType = pathinfo($_FILES[$uploadedTo]["name"],PATHINFO_EXTENSION);
 			$totalImgDir = $target_file . "." . $imageFileType;
 			// Check if image file is a actual image or fake image
 			if(isset($_POST["submit"])) {
-				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+				$check = getimagesize($_FILES[$uploadedTo]["tmp_name"]);
 			if($check !== false and in_array(strtolower($imageFileType),array("gif","jpg","jpeg","png")) ) {
-				if ($_FILES["fileToUpload"]["size"] > 10485760) {
+				if ($_FILES[$uploadedTo]["size"] > 10485760) {
 					return array(false,"File is larger than 10MB");
 				}
-				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$totalImgDir)) {
+				if (move_uploaded_file($_FILES[$uploadedTo]["tmp_name"],$totalImgDir)) {
 					return array(True,"File Successfully Uploaded!",$totalImgDir);
 				} else {
 					return array(False,"Error While Uploading File");
@@ -47,11 +47,16 @@ function isImageWithUpload($user) {
 }
 }
 
-function generateAllSizesOfImage($origloc) {
-	$newhires = str_replace("orig","256",$origloc);
+function generateAllSizesOfImage($origloc,$replTxt) {
+	if (!isset($replTxt)) {
+		$replTxt = "orig";
+	} else if (strlen($replTxt) < 1) {
+		$replTxt = "orig";
+	}
+	$newhires = str_replace($replTxt,"256",$origloc);
 	img_resize($origloc,$newhires,256,256);
-	$newmidres = str_replace("orig","128",$origloc);
-	$newlores = str_replace("orig","64",$origloc);
+	$newmidres = str_replace($replTxt,"128",$origloc);
+	$newlores = str_replace($replTxt,"64",$origloc);
 	img_resize($origloc,$newmidres,128,128);
 	img_resize($origloc,$newlores,64,64);
 }
